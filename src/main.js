944d298caf32ecf522785611929af7c4e27f2fff
Ilya <kudryavcev.k.i.s@gmail.com>
2019-05-04 01:01:40 +0300

    added a matrix and simplex class

--- src/main.js
+++ src/main.js
@@ -1,13 +1,47 @@
 const FileSystem = require('./Utils/FileSystem');
+const Simplex = require('./Math/Simplex');
 
 function start() {
     const pathToConfig = process.argv[2];
     const config = JSON.parse(FileSystem.readFile(pathToConfig));
 
-    const result = {
-        Name: 'Ilya',
-        comment: 'I am good student'
-    };
+    const source = config.simplex.reduce((result, element, index) => {
+        const line = element.replace(/[\s]*/g, '').split(',');
+
+        result[index] = [];
+
+        for (const value of line) {
+            result[index].push(+value);
+        }
+
+        return result;
+    }, {});
+
+    const simplex = new Simplex(source);
+
+    let sumElement = 0;
+
+    simplex.each(element => {
+        sumElement += element;
+    });
+
+    const elements = [];
+
+    simplex.each((element, numberLine, numberColumn, line, column) => {
+        elements.push({
+            value: element,
+            numberLine: numberLine,
+            numberColumn: numberColumn,
+            line: line,
+            column: column
+        })
+    });
+
+    result = {
+        matrix: simplex.get(),
+        elements: elements,
+        sumElement: sumElement
+    }
 
     FileSystem.writeFile(config.pathToResult, result, {uncoding: 'unf8'});
 
