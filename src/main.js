1b07cd23bb05e0efe225a66bb4c6c2b3000f46a5
Ilya <kudryavcev.k.i.s@gmail.com>
2019-05-12 01:40:04 +0300

    added lagrange

--- src/main.js
+++ src/main.js
@@ -22,7 +22,13 @@ function start() {
     const trans = simplex.transposed;
 
     const revers = simplex.revers;
-
+    
+    const norm = simplex.normProjectora({
+        0: [0, 0],
+        1: [0, 1],
+        2: [1, 0],
+        3: [1, 1]
+    });
     FileSystem.writeFile(config.pathToResult, revers.toString(), {uncoding: 'unf8'});
 
     console.log(config.text);
